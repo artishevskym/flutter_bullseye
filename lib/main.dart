865d678c4a44d6b2cfccbfc4fb5c4515b3f7e803
Martin Artishevsky <artishevsky.m@gmail.com>
2021-02-16 19:40:58 +0000

    Add Hit Me button and show popup

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ void main() => runApp(BullsEyeApp());
 
 /*
 TO-DO LIST
-- Add the "Hit Me" button
-- Show a popup when the user taps it
++ Add the "Hit Me" button
++ Show a popup when the user taps it
 - Put game info on screen
 - Put slider on screen: 1->100
 - Read value of the slider
@@ -38,6 +38,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,10 +55,36 @@ class _GamePageState extends State<GamePage> {
               FlatButton(
                   onPressed: () {
                     print("Button pressed!");
+                    this._alertIsVisible = true;
+                    _showAlert(context);
                   },
                   child: Text('Hit Me!', style: TextStyle(color: Colors.blue)))
             ]),
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+        child: Text("Awesome!"),
+        onPressed: () {
+          Navigator.of(context).pop();
+          this._alertIsVisible = false;
+          print("Awesome pressed! $_alertIsVisible");
+        });
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Hello there!"),
+          content: Text("This is my first pop-up."),
+          actions: <Widget>[
+            okButton,
+          ],
+          elevation: 5,
+        );
+      },
     );
   }
 }
